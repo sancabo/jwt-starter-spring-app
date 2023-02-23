78725f0194caf695fea92bd02ee0a12b386983d2
Santiago Cabo <newsancabo@gmail.com>
2023-02-22 22:25:17 -0300

    Changed project name

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ An example of handling Jason Web Tokens with Spring Security
 It exposes an authentication endpoint and resource endpoints.
 
 Authentication endpoint returns a Jason Web token. Valid for 2 minutes.
+
--- src/test/java/com/jwt/app/JWTWorkshopApplicationTest.java
+++ src/test/java/com/jwt/app/JWTWorkshopApplicationTest.java
@@ -1,10 +1,10 @@
-package com.platzi.market;
+package com.jwt.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PlatziMarketApplicationTests {
+class JWTWorkshopApplicationTest {
 
 	@Test
 	void contextLoads() {
